6b63477fe78b9b8aef0140ac8ee1b2737feb1b77
Bill Woodward <bill.woodward@rackspace.com>
2014-10-16 10:05:51 -0400

    Move from a callback-based architecture to a flux-based architecture

--- js/main.js
+++ js/main.js
@@ -1,30 +1,99 @@
 /** @jsx React.DOM */
 
+var RegionDispatcher = function () {
+  Dispatcher.call(this);
+};
+RegionDispatcher.prototype = Object.create(Dispatcher.prototype);
+
+RegionDispatcher.prototype.handleViewAction = function (action) {
+  this.dispatch({
+    action: action
+  });
+};
+
+var regionDispatcher = new RegionDispatcher();
+
+var RegionStore = function () {
+  EventEmitter.call(this);
+
+  this._state = {
+    title: 'All Regions (Global)',
+    visible: false,
+    region: 'ALL'
+  }
+};
+RegionStore.prototype = Object.create(EventEmitter.prototype);
+
+RegionStore.prototype.emitChange = function () {
+  this.emit('change');
+};
+
+RegionStore.prototype.addChangeListener = function (callback) {
+  this.on('change', callback);
+};
+
+RegionStore.prototype.removeChangeListener = function (callback) {
+  this.removeListener('change', callback);
+};
+
+RegionStore.prototype.setVisibility = function (visible) {
+  this._state.visible = visible;
+};
+
+RegionStore.prototype.selectRegion = function (title, region) {
+  this._state.title = title;
+  this._state.region = region;
+};
+
+RegionStore.prototype.getState = function () {
+  return this._state;
+};
+
+RegionStore.prototype.register = regionDispatcher.register(function (payload) {
+  var action = payload.action;
+
+  switch(action.actionType) {
+    case 'show menu':
+      regionStore.setVisibility(true);
+      regionStore.emitChange();
+      break;
+
+    case 'hide menu':
+      regionStore.setVisibility(false);
+      regionStore.emitChange();
+      break;
+
+    case 'select':
+      regionStore.selectRegion(action.title, action.region);
+      regionStore.emitChange();
+      break;
+  }
+});
+
+RegionStore.prototype._state = {};
+
+var regionStore = new RegionStore();
+
 var ActionMenuButton = React.createClass({
   getInitialState: function () {
-    return {
-      title: 'All Regions (Global)',
-      visible: false,
-      selected: 'ALL'
-    };
+    return regionStore.getState();
   },
-  showMenu: function (e) {
-    this.setState({visible: true});
+  _onChange: function () {
+    console.log('Received a change event');
+    console.log('State = ' + regionStore.getState().title);
+    this.setState(regionStore.getState());
   },
-  hideMenu: function (e) {
-    this.setState({visible: false});
+  componentDidMount: function () {
+    regionStore.addChangeListener(this._onChange);
   },
-  selectItem: function (e) {
-    this.setState({
-      title: e.currentTarget.getAttribute('title'),
-      selected: e.currentTarget.getAttribute('value')
-    });
+  componentWillUnmount: function () {
+    regionStore.removeChangeListener(this._onChange);
   },
   render: function () {
     return (
       <div className="rs-dropdown">
-        <ActionButton title={this.state.title} showMenu={this.showMenu} />
-        <ActionMenu visible={this.state.visible} selected={this.state.selected} hideMenu={this.hideMenu} select={this.selectItem}>
+        <ActionButton title={this.state.title} />
+        <ActionMenu visible={this.state.visible} selectedRegion={this.state.region}>
           <GlobalRegion title="All Regions (Global)" value="ALL" />
           <GeographicalRegion title="United States" value="US" />
           <LocalRegion title="Northern Virginia (IAD)" value="IAD" />
@@ -42,9 +111,14 @@ var ActionMenuButton = React.createClass({
 });
 
 var ActionButton = React.createClass({
+  _showMenu: function () {
+    regionDispatcher.handleViewAction({
+      actionType: 'show menu'
+    });
+  },
   render: function () {
     return (
-      <button className="rs-btn rs-btn-action rs-dropdown-toggle" onClick={this.props.showMenu}>
+      <button className="rs-btn rs-btn-action rs-dropdown-toggle" onClick={this._showMenu}>
         <span className="rs-cog"></span> {this.props.title} <span className="rs-caret"></span>
       </button>
     )
@@ -52,19 +126,23 @@ var ActionButton = React.createClass({
 });
 
 var ActionMenu = React.createClass({
+  _hideMenu: function () {
+    regionDispatcher.handleViewAction({
+      actionType: 'hide menu'
+    });
+  },
   componentWillUnmount: function (e) {
     $('body').off('click');
   },
   componentDidMount: function () {
-    $('body').on('click', this.props.hideMenu);
+    $('body').on('click', this._hideMenu);
   },
   render: function () {
     var visible = this.props.visible ? 'visible' : 'hidden';
     var children = React.Children.map(this.props.children, function (child) {
       return React.addons.cloneWithProps(child, {
-        selected: (child.props.value === this.props.selected),
-        select: this.props.select}
-      );
+        selected: (child.props.value === this.props.selectedRegion),
+      });
     }, this);
 
     return (
@@ -74,6 +152,13 @@ var ActionMenu = React.createClass({
 });
 
 var GlobalRegion = React.createClass({
+  _selectItem: function (e) {
+    regionDispatcher.handleViewAction({
+      actionType: 'select',
+      title: this.props.title,
+      region: this.props.value
+    });
+  },
   render: function () {
     var selectedClass = this.props.selected ? 'selected' : '';
 
@@ -81,7 +166,7 @@ var GlobalRegion = React.createClass({
       <li className={"rs-dropdown-item " + selectedClass}>
         <span className="region-checkbox"></span>
         <span className="region global-region"
-          onClick={this.props.select}
+          onClick={this._selectItem}
           value={this.props.value}
           title={this.props.title}>{this.props.title}</span>
       </li>
@@ -90,6 +175,13 @@ var GlobalRegion = React.createClass({
 });
 
 var GeographicalRegion = React.createClass({
+  _selectItem: function (e) {
+    regionDispatcher.handleViewAction({
+      actionType: 'select',
+      title: this.props.title,
+      region: this.props.value
+    });
+  },
   render: function () {
     var selectedClass = this.props.selected ? 'selected' : '';
 
@@ -97,7 +189,7 @@ var GeographicalRegion = React.createClass({
       <li className={"rs-dropdown-item " + selectedClass}>
         <span className="region-checkbox"></span>
         <span className="region geographical-region"
-          onClick={this.props.select}
+          onClick={this._selectItem}
           value={this.props.value}
           title={this.props.title}>{this.props.title}</span>
       </li>
@@ -106,6 +198,13 @@ var GeographicalRegion = React.createClass({
 });
 
 var LocalRegion = React.createClass({
+  _selectItem: function (e) {
+    regionDispatcher.handleViewAction({
+      actionType: 'select',
+      title: this.props.title,
+      region: this.props.value
+    });
+  },
   render: function () {
     var selectedClass = this.props.selected ? 'selected' : '';
 
@@ -113,7 +212,7 @@ var LocalRegion = React.createClass({
       <li className={"rs-dropdown-item " + selectedClass}>
         <span className="region-checkbox"></span>
         <span className="region local-region"
-          onClick={this.props.select}
+          onClick={this._selectItem}
           value={this.props.value}
           title={this.props.title}>{this.props.title}</span>
       </li>
